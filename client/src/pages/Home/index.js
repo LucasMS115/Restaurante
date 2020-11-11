b8f287e5c6f60ed191f165388b2d4b6df17024a2
gi-armani <giovana_armani@usp.br>
2020-11-11 15:58:58 -0300

    Criacao da parte de nossa especialidade no home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,6 +7,13 @@ const home = (props) => {
     return(
         <div id="title">
             <Header/>
+
+            <div className="especialidade">
+                <h1>Nossa especialidade</h1>
+                <span className="separator">Alguma Coisa</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque, lorem ut malesuada mattis, ante nisl ultricies orci, ut scelerisque nulla nibh non ipsum. Duis nec condimentum nulla. Cras congue dui mi, eget elementum sapien finibus vel. Quisque at nibh massa. Nunc purus ex, tempus sed metus a, maximus feugiat lacus. Mauris fringilla pharetra consequat. In suscipit nibh sed est rhoncus, eget dapibus sapien tincidunt.</p>
+            </div>
+
             <Footer/>
         </div>
     );
